c9f5ecb4b7d35c4def3f2dc679cfcd4c93c9d044
lauramolez <lauramorelez@gmail.com>
2022-02-04 14:46:56 +0100

    home

--- healthup/src/app/app-routing.module.ts
+++ healthup/src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }, { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'logIn', loadChildren: () => import('./log-in/log-in.module').then(m => m.LogInModule) }, { path: 'signUp', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) }, { path: 'userDashboard', loadChildren: () => import('./user-dashboard/user-dashboard.module').then(m => m.UserDashboardModule) }, { path: 'contactUs', loadChildren: () => import('./contact-us/contact-us.module').then(m => m.ContactUsModule) }];
+const routes: Routes = [
+  {
+    path: 'shared',
+    loadChildren: () =>
+      import('./shared/shared.module').then((m) => m.SharedModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'logIn',
+    loadChildren: () =>
+      import('./log-in/log-in.module').then((m) => m.LogInModule),
+  },
+  {
+    path: 'signUp',
+    loadChildren: () =>
+      import('./sign-up/sign-up.module').then((m) => m.SignUpModule),
+  },
+  {
+    path: 'userDashboard',
+    loadChildren: () =>
+      import('./user-dashboard/user-dashboard.module').then(
+        (m) => m.UserDashboardModule
+      ),
+  },
+  {
+    path: 'contactUs',
+    loadChildren: () =>
+      import('./contact-us/contact-us.module').then((m) => m.ContactUsModule),
+  },
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

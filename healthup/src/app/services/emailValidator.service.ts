e52f29a65d7d1c6967d5b60cd31c9aba80b33eae
lauramolez <lauramorelez@gmail.com>
2022-02-11 21:51:49 +0100

    arreglao

--- healthup/src/app/services/emailValidator.service.ts
+++ healthup/src/app/services/emailValidator.service.ts
@@ -1,11 +1,11 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
   AbstractControl,
   AsyncValidator,
   ValidationErrors,
 } from '@angular/forms';
-import { catchError, debounceTime, map, Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, debounce, debounceTime, delay, map, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { AuthServiceService } from './auth-service.service';
 import { User } from '../interfaces/interfaces';
@@ -14,35 +14,34 @@ import { User } from '../interfaces/interfaces';
   providedIn: 'root',
 })
 export class emailValidatorService implements AsyncValidator{
-  constructor(
-    private http: HttpClient,
-    private authService: AuthServiceService
-  ) {}
+  constructor(private http: HttpClient) {}
+
   private baseUrl: string = environment.baseUrl;
 
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
-
    const email = control.value;
-/*
-    let token = JSON.parse(<string>localStorage.getItem('jwt'));*/
-    const url = `${this.baseUrl}/users/${email}`;
-    //const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);//.set('Access-Control-Allow-Origin','*')
-
-    return this.http.get<User>(url);//subscribe
-      // delay(3000),
-    /*  debounceTime(300),//para que no lo comprueba al momento (no comprueba letra a letra)
-      map( resp => {
-        return ( resp.length === 0 )
-            ? null
-            : { emailTomado: true }
-      })
-
-    );*/
+   return this.compruebaEmail(email).pipe(
+     map (resp => {
+       console.log(resp);
+       if(resp.email != null){
+        console.log(resp);
+          return {emailIndicado: true};
+       }else{
+        return null;
+       }
+     }),
+     catchError (err => {
+       console.log('ERRO EMAIL');
+        console.log(err);
+        return of(null);
+     })
+   );
   }
 
+  //Método para comprobar email en BBDD
   compruebaEmail(email:string){
     const url = `${this.baseUrl}/users/${email}`;
-    return this.http.get<User>(url);//subscribe
+    return this.http.get<User>(url);
   }
 
 
